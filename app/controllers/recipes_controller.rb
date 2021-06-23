860135b0f9d2dfe4325ff8f7310e17547e1435e6
Naomi-Rhames <naomirhames2020@gmail.com>
2021-06-23 01:33:15 -0500

    commit tree c2c556613ba42041786abe51880b60f82680c97d

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,19 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+   10.times {@recipe.ingredients.build}
+    
   end
 
   def create
+    @recipe = Recipe.new(recipie_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe), notice: "Your recipie was created"
+    end
+  end
+  private
+  def recipie_params
+    params.require(:recipe).permit(:title, 
+      ingredients_attributes: [ :quantity, :name])
   end
 end
